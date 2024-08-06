c8fbd2a80365b4603b9d39eb64b6b770d443af76
Dannie <DannieSchumaker@lithia.com>
2024-08-06 11:34:29 -0500

    Updated date format on order submitted page

--- src/components/OrderSubmitted.tsx
+++ src/components/OrderSubmitted.tsx
@@ -1,17 +1,27 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { OrderContext } from "../stores/OrderProvider";
+import { useNavigate } from "react-router-dom";
 import "./OrderSubmitted.css";
 
 const OrderSubmitted = () => {
   const {
     orderReadyTime,
   } = useContext(OrderContext)
+    const navigate = useNavigate();
 
-  // TODO redirect or display error if orderReadyTime is null
+  useEffect(() => {
+    if (!orderReadyTime) {
+      navigate("/")
+    }
+  }, [orderReadyTime, navigate])
+
+  // TODO validate date
+  const date = new Date(orderReadyTime!!).toTimeString()
+
   return (
     <section className={"Order-page"}>
       <h1>Order Submitted</h1>
-      <p>Your order will be ready for pickup at {orderReadyTime}</p>
+      <p>Your order will be ready for pickup at {date}</p>
     </section>
   );
 };
